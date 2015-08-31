3baa040e72e26c1434e8aff0ef14922c9ccf59aa
fkunneman <f.kunneman@let.ru.nl>
2015-08-31 16:04:07 +0200

    added linewriter

---
+++ linewriter.py
@@ -0,0 +1,37 @@
+
+import xlwt
+import re
+
+import gen_functions
+
+class Linewriter:
+
+    def __init__(self, lines):
+        self.lines = lines
+
+    def write_xls(self, headers, header_style, outfile):
+        if len(self.lines) > 65535:
+            num_chunks = int(len(self.lines) / 65534) + 1
+            chunks = gen_functions.make_chunks(self.lines, nc = num_chunks)
+        else:
+            chunks = [self.lines]
+        book = xlwt.Workbook()
+        algn1 = xlwt.Alignment()
+        algn1.wrap = 1
+        style = xlwt.XFStyle()
+        style.alignment = algn1
+        for sheetno, chunk in enumerate(chunks):
+            tabname = 'sheet_' + str(sheetno)
+            tab = book.add_sheet(tabname)
+            for i, header in enumerate(headers):
+                tab.write(0, i, header, style)
+            for i, line in enumerate(chunk):
+                i += 1
+                for j, column in enumerate(line):
+                    if re.search('^http', str(column)):
+                        url = 'HYPERLINK(\"' + column + '\"; \"' + column + '\")'
+                        tab.write(i, j, xlwt.Formula(url))
+                    else:
+                        style.num_format_str = header_style[headers[j]]
+                        tab.write(i, j, column, style)
+        book.save(outfile)
